063c12fed71c3fda939b7f8b2ed7638005301b66
jacksterdealeo <oswaldtechnic@zoho.com>
2025-09-06 05:25:24 -0800

    fixed comments

--- src/extractlinksandimages.py
+++ src/extractlinksandimages.py
@@ -2,12 +2,12 @@ import re
 
 
 # takes text:string
-# returns list[tuple(alt_text:string, URL:string)] or raises exception
+# returns list[tuple(alt_text:string, URL:string)]
 def extract_markdown_images(text):
     return re.findall(r"!\[([^\[\]]*)\]\(([^\(\)]*)\)", text)
 
 
 # takes text:string
-# returns list[tuple(anchor_text:string, URL:string)] or raises exception
+# returns list[tuple(anchor_text:string, URL:string)]
 def extract_markdown_links(text):
     return re.findall(r"(?<!!)\[([^\[\]]*)\]\(([^\(\)]*)\)", text)
