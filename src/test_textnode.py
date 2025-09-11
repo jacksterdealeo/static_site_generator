0fae7f2c6889cbf16a5383cd0a9f996ed32fb9a3
jacksterdealeo <oswaldtechnic@zoho.com>
2025-09-11 00:17:33 -0800

    CH3 L6: Text to TextNodes

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -1,6 +1,6 @@
 import unittest
 
-from textnode import TextNode, TextType
+from textnode import TextNode, TextType, text_to_textnodes
 
 
 class TestTextNode(unittest.TestCase):
@@ -28,6 +28,50 @@ class TestTextNode(unittest.TestCase):
     def test_none_link(self):
         node = TextNode("This is a text node", TextType.LINK)
         self.assertEqual(None, node.url)
+
+    def test_text_to_textnodes_default(self):
+        text = "This is **text** with an _italic_ word and a `code block` and an ![obi wan image](https://i.imgur.com/fJRm4Vk.jpeg) and a [link](https://boot.dev)"
+        nodes = text_to_textnodes(text)
+        self.assertEqual(
+            nodes,
+            [
+                TextNode("This is ", TextType.TEXT),
+                TextNode("text", TextType.BOLD),
+                TextNode(" with an ", TextType.TEXT),
+                TextNode("italic", TextType.ITALIC),
+                TextNode(" word and a ", TextType.TEXT),
+                TextNode("code block", TextType.CODE),
+                TextNode(" and an ", TextType.TEXT),
+                TextNode("obi wan image", TextType.IMAGE, "https://i.imgur.com/fJRm4Vk.jpeg"),
+                TextNode(" and a ", TextType.TEXT),
+                TextNode("link", TextType.LINK, "https://boot.dev"),
+            ]
+        )
+
+    def test_text_to_textnodes_bold_only(self):
+        text = "**BOLD ME FATHER**"
+        nodes = text_to_textnodes(text)
+        self.assertEqual(
+            nodes,
+            [
+                TextNode("BOLD ME FATHER", TextType.BOLD),
+            ]
+        )
+
+    def test_text_to_textnodes_text_only(self):
+        text = "Only normal human words are here."
+        nodes = text_to_textnodes(text)
+        self.assertEqual(
+            nodes,
+            [
+                TextNode("Only normal human words are here.", TextType.TEXT),
+            ]
+        )
+
+    def test_text_to_textnodes_nothing(self):
+        text = ""
+        nodes = text_to_textnodes(text)
+        self.assertEqual(nodes, [])
 
 
 if __name__ == "__main__":
--- src/textnode.py
+++ src/textnode.py
@@ -1,5 +1,5 @@
+import re
 from enum import Enum
-from htmlnode import HTMLNode
 from leafnode import LeafNode
 
 
@@ -32,7 +32,7 @@ class TextNode():
 
 
 # takes old_nodes:list[TextNode], dilimiter:string, text_type:TextType
-# returns list[TextNode] or raises exception
+# returns list[TextNode]
 def split_nodes_delimiter(old_nodes, delimiter, text_type):
     result = []
     for old_node in old_nodes:
@@ -55,6 +55,25 @@ def split_nodes_delimiter(old_nodes, delimiter, text_type):
     return result
 
 
+# takes text:string
+# returns list[TextNode]
+def text_to_textnodes(text):
+    nodes = [TextNode(text, TextType.TEXT)]
+    nodes = split_nodes_image(nodes)
+    nodes = split_nodes_link(nodes)
+    delimiters = [
+        ("**", TextType.BOLD),
+        ("_", TextType.ITALIC),
+        ("`", TextType.CODE)
+    ]
+    for d in delimiters:
+        nodes = (split_nodes_delimiter(nodes, d[0], d[1]))
+
+    return nodes
+
+
+# takes text_node:TextNode
+# returns LeafNode
 def text_node_to_html_node(text_node):
     match text_node.text_type:
         case TextType.TEXT:
@@ -68,6 +87,72 @@ def text_node_to_html_node(text_node):
         case TextType.LINK:
             return LeafNode("a", text_node.text, "href")
         case TextType.IMAGE:
-            return LeafNode("img", None, {"src": text_node.url, "alt": text_node.text,})
+            return LeafNode("img", None, {"src": text_node.url, "alt": text_node.text})
         case _:
             pass
+
+
+# takes text:string
+# returns list[tuple(alt_text:string, URL:string)]
+def extract_markdown_images(text):
+    return re.findall(r"!\[([^\[\]]*)\]\(([^\(\)]*)\)", text)
+
+
+# takes text:string
+# returns list[tuple(anchor_text:string, URL:string)]
+def extract_markdown_links(text):
+    return re.findall(r"(?<!!)\[([^\[\]]*)\]\(([^\(\)]*)\)", text)
+
+
+# takes list[TextNode]
+# returns list[TextNode]
+def split_nodes_image(old_nodes):
+    result = []
+    for old_node in old_nodes:
+        if old_node.text_type != TextType.TEXT:
+            result.append(old_node)
+            continue
+        remaining_text = old_node.text
+        images = extract_markdown_images(remaining_text)
+        if len(images) == 0:
+            result.append(old_node)
+            continue
+        nodes_from_node = []
+        for image in images:
+            image_alt, image_link = image
+            text_segments = remaining_text.split(f"![{image_alt}]({image_link})", 1)
+            if len(text_segments[0]) != 0:
+                nodes_from_node.append(TextNode(text_segments[0], TextType.TEXT))
+            nodes_from_node.append(TextNode(image_alt, TextType.IMAGE, url=image_link))
+            remaining_text = text_segments[1]
+        if len(remaining_text) != 0:
+            nodes_from_node.append(TextNode(remaining_text, TextType.TEXT))
+        result.extend(nodes_from_node)
+    return result
+
+
+# takes list[TextNode]
+# returns list[TextNode]
+def split_nodes_link(old_nodes):
+    result = []
+    for old_node in old_nodes:
+        if old_node.text_type != TextType.TEXT:
+            result.append(old_node)
+            continue
+        remaining_text = old_node.text
+        links = extract_markdown_links(remaining_text)
+        if len(links) == 0:
+            result.append(old_node)
+            continue
+        nodes_from_node = []
+        for link in links:
+            link_text, link_url = link
+            text_segments = remaining_text.split(f"[{link_text}]({link_url})", 1)
+            if len(text_segments[0]) != 0:
+                nodes_from_node.append(TextNode(text_segments[0], TextType.TEXT))
+            nodes_from_node.append(TextNode(link_text, TextType.LINK, url=link_url))
+            remaining_text = text_segments[1]
+        if len(remaining_text) != 0:
+            nodes_from_node.append(TextNode(remaining_text, TextType.TEXT))
+        result.extend(nodes_from_node)
+    return result
