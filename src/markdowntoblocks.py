9c294b2e91a6048682bb749b5b602ab7e6bdfc72
jacksterdealeo <oswaldtechnic@zoho.com>
2025-09-14 03:08:46 -0800

    CH4 L2: Block Types

--- src/markdowntoblocks.py
+++ src/markdowntoblocks.py
@@ -1,3 +1,6 @@
+from enum import Enum
+import re
+
 from textnode import TextNode
 from textnode import text_to_textnodes
 
@@ -13,3 +16,39 @@ def markdown_to_blocks(markdown):
             continue
         result.append(block)
     return result
+
+
+class BlockType(Enum):
+    PARAGRAPH = "paragraph"
+    HEADING = "heading"
+    CODE = "code"
+    QUOTE = "quote"
+    UNORDERED_LIST = "unordered_list"
+    ORDERED_LIST = "ordered_list"
+
+
+# takes markdown:string
+# returns BlockType
+def block_to_block_type(markdown):
+    pass
+    if re.match(r"(#){1,6}( )", markdown) is not None:
+        return BlockType.HEADING
+    if re.match(r"(`){3}", markdown) is not None and re.search(r"((`){3}$)", markdown) is not None:
+        return BlockType.CODE
+    if re.fullmatch(r"(?m)^>.*(?:\n>.*)*\n?$", markdown) is not None:
+        return BlockType.QUOTE
+    if re.fullmatch(r"(?m)^(- ).*(?:\n(- ).*)*\n?$", markdown) is not None:
+        return BlockType.UNORDERED_LIST
+
+    is_ordered_list = True
+    count = 1
+    for line in markdown.split("\n"):
+        if line.startswith(f"{count}. "):
+            count += 1
+        else:
+            is_ordered_list = False
+            break
+    if is_ordered_list:
+        return BlockType.ORDERED_LIST
+
+    return BlockType.PARAGRAPH
--- src/test_markdowntoblocks.py
+++ src/test_markdowntoblocks.py
@@ -1,6 +1,6 @@
 import unittest
 
-from markdowntoblocks import markdown_to_blocks
+from markdowntoblocks import markdown_to_blocks, block_to_block_type, BlockType
 
 
 class TestLeafNode(unittest.TestCase):
@@ -22,4 +22,69 @@ This is the same paragraph on a new line
                 "This is another paragraph with _italic_ text and `code` here\nThis is the same paragraph on a new line",
                 "- This is a list\n- with items",
             ],
+        )
+
+    def test_block_to_block_type_blank(self):
+        md = ""
+        block_type = block_to_block_type(md)
+        self.assertEqual(
+            block_type,
+            BlockType.PARAGRAPH
+        )
+
+    def test_block_to_block_type_heading(self):
+        md = """#### TEST
+qwqweqdas asdaw"""
+        block_type = block_to_block_type(md)
+        self.assertEqual(
+            block_type,
+            BlockType.HEADING
+        )
+
+    def test_block_to_block_type_code(self):
+        md = """```sfvsdfrgf !$#%^@#$)()
+dfsfs
+sa@SADAW$43fqaw```"""
+        block_type = block_to_block_type(md)
+        self.assertEqual(
+            block_type,
+            BlockType.CODE
+        )
+
+    def test_block_to_block_type_quote(self):
+        md = """>GEORGE WAS IN A WELL."""
+        block_type = block_to_block_type(md)
+        self.assertEqual(
+            block_type,
+            BlockType.QUOTE
+        )
+
+    def test_block_to_block_type_unordered_list(self):
+        md = """- sfdwerfsdf
+- sdfwsfef
+- dgfdgrs"""
+        block_type = block_to_block_type(md)
+        self.assertEqual(
+            block_type,
+            BlockType.UNORDERED_LIST
+        )
+
+    def test_block_to_block_type_ordered_list(self):
+        md = """1. sfdwerfsdf
+2. sdfwsfef
+3. dgfdgrs"""
+        block_type = block_to_block_type(md)
+        self.assertEqual(
+            block_type,
+            BlockType.ORDERED_LIST
+        )
+
+    def test_block_to_block_type_ordered_list_broken1(self):
+        md = """1. sfdwerfsdf
+2. sdfwsfef
+4. dgfd"""
+        block_type = block_to_block_type(md)
+        self.assertNotEqual(
+            block_type,
+            BlockType.ORDERED_LIST
         )
