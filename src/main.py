82faa539cef74855c9bd9364eaacf5f44254fdab
jacksterdealeo <oswaldtechnic@zoho.com>
2025-10-04 23:58:52 -0800

    CH5 L1: Copy Static

--- src/main.py
+++ src/main.py
@@ -1,8 +1,13 @@
+import os
+from shutil import copy
+
 from textnode import TextNode
 from textnode import TextType
 
 
 def main():
+    delete("./public")
+    copy_rec("./static", "./public")
     node = TextNode(
         "Hello, Larl.",
         TextType.LINK,
@@ -10,6 +15,42 @@ def main():
     )
     print(node)
 
+def delete(folder_path: str):
+    if not os.path.exists(folder_path):
+        return
+    for filename in os.listdir(folder_path):
+        print(f"Deleting: {filename}")
+        file_path = os.path.join(folder_path, filename)  
+        try:
+            if os.path.isfile(file_path):
+                os.remove(file_path)  
+            elif os.path.isdir(file_path):  
+                os.rmdir(file_path)  
+        except Exception as e:  
+            print(f"Error deleting {file_path}: {e}")
+    print("Deletion done")
+
+def copy_by_tree(src: str, dst: str) -> None:
+    os.makedirs(dst, exist_ok=True)
+    for root, dirs, files in os.walk(src):
+        rel = os.path.relpath(root, src)
+        target_dir = os.path.join(dst, rel)
+        os.makedirs(target_dir, exist_ok=True)
+        for file in files:
+            copy(os.path.join(root, file), os.path.join(target_dir, file))
+
+def copy_rec(src: str, dst: str) -> None:
+    src = os.path.abspath(src)
+    dst = os.path.abspath(dst)
+    if not os.path.exists(dst):
+        os.mkdir(dst)
+    for path in os.listdir(src):
+        full_path = os.path.join(src, path)
+        if os.path.isfile(full_path):
+            copy(full_path, dst)
+        elif os.path.isdir(full_path):
+            copy_rec(full_path, os.path.join(dst, path))
+    print(f"Copying to {dst} done")
 
 if __name__ == "__main__":
     main()
